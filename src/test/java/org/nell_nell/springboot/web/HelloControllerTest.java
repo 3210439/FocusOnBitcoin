ab6055effe91fd9f8a9b94c53b99cc2ab7d76b83
Godric <kimdeiz@naver.com>
2021-12-24 23:13:57 +0900

    HelloController와 HelloControllerTest
    MockMvc클래스를 통해서 POST,GET 요청에 대하여 테스트 할 수 있다.

---
+++ src/test/java/org/nell_nell/springboot/web/HelloControllerTest.java
@@ -0,0 +1,34 @@
+package org.nell_nell.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.Assert.*;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    // MockMvc 클래스 덕분에 GET, POST 등의 요청에대한 API 테스트를 할 수 있다.
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
