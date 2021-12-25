7c62cee14f54db588d715aea784babd2eaec8f7b
Godric <kimdeiz@naver.com>
2021-12-25 17:35:09 +0900

    4.5 삭제기능 구현

--- src/main/java/org/nell_nell/springboot/web/IndexController.java
+++ src/main/java/org/nell_nell/springboot/web/IndexController.java
@@ -2,9 +2,11 @@ package org.nell_nell.springboot.web;
 
 import lombok.RequiredArgsConstructor;
 import org.nell_nell.springboot.service.posts.PostsService;
+import org.nell_nell.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -24,5 +26,13 @@ public class IndexController {
     public String postsSave() {
         return "posts-save";
 
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
