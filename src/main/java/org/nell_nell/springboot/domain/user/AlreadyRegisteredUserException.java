135525fe1d30990240767dac827c2c3ead1cc19a
Godric <kimdeiz@naver.com>
2022-02-17 00:10:17 +0900

    gitflow-feature-stash: for_security_login

---
+++ src/main/java/org/nell_nell/springboot/domain/user/AlreadyRegisteredUserException.java
@@ -0,0 +1,12 @@
+package org.nell_nell.springboot.domain.user;
+
+public class AlreadyRegisteredUserException extends RuntimeException {
+
+    public AlreadyRegisteredUserException(String message) {
+        super(message);
+    }
+
+    public AlreadyRegisteredUserException() {
+        super("이미 등록된 유저입니다.");
+    }
+}
