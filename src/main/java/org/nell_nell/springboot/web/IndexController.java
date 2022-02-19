cfa5f17004716fafb58f5d4f19f298f4758d9613
Godric <kimdeiz@naver.com>
2022-02-19 16:51:19 +0900

    gitflow-feature-stash: security로그인구현

--- src/main/java/org/nell_nell/springboot/web/IndexController.java
+++ src/main/java/org/nell_nell/springboot/web/IndexController.java
@@ -3,10 +3,12 @@ package org.nell_nell.springboot.web;
 import lombok.RequiredArgsConstructor;
 import org.nell_nell.springboot.config.auth.LoginUser;
 import org.nell_nell.springboot.config.auth.dto.SessionUser;
+import org.nell_nell.springboot.domain.user.User;
 import org.nell_nell.springboot.service.article.ArticleService;
 import org.nell_nell.springboot.service.posts.PostsService;
 import org.nell_nell.springboot.web.dto.PostsResponseDto;
 import org.nell_nell.springboot.web.dto.article_dto.ArticleResponseDto;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,20 +25,26 @@ public class IndexController {
     //private final HttpSession httpSession;
 
     @GetMapping("/index")
-    public String index(Model model, @LoginUser SessionUser user) {
+    public String index(Model model, @LoginUser SessionUser user, @AuthenticationPrincipal User user_s) {
         // 가져온 결과를 posts로 전달한다.
         model.addAttribute("posts", postsService.findAllDesc());
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
+        if (user_s != null) {
+            model.addAttribute("nickname", user_s.getName());
+        }
         return "index";
     }
 
     @GetMapping("/")
-    public String main(Model model, @LoginUser SessionUser user) {
+    public String main(Model model, @LoginUser SessionUser user, @AuthenticationPrincipal User user_s) {
         if (user != null) {
-            model.addAttribute("userName", user.getName());
+            model.addAttribute("nickname", user.getName());
+        }
+        if (user_s != null) {
+            model.addAttribute("nickname", user_s.getName());
         }
         return "main";
     }
@@ -47,15 +55,18 @@ public class IndexController {
     }
 
     @GetMapping("/humorBoard")
-    public String humorBoard(Model model, @LoginUser SessionUser user)
+    public String humorBoard(Model model, @LoginUser SessionUser user, @AuthenticationPrincipal User user_s)
     {
         // 가져온 결과를 posts로 전달한다.
         model.addAttribute("article", articleService.findAllDesc());
 
-        if (user != null)
-        {
-            model.addAttribute("userName", user.getName());
+        if (user != null) {
+            model.addAttribute("nickname", user.getName());
         }
+        if (user_s != null) {
+            model.addAttribute("nickname", user_s.getName());
+        }
+
         return "humorBoard";
     }
 
@@ -65,11 +76,14 @@ public class IndexController {
 
     }
     @GetMapping("/article/save")
-    public String articleSave(Model model, @LoginUser SessionUser user) {
-        if (user != null)
-        {
-            model.addAttribute("userName", user.getName());
+    public String articleSave(Model model, @LoginUser SessionUser user, @AuthenticationPrincipal User user_s) {
+        if (user != null) {
+            model.addAttribute("nickname", user.getName());
         }
+        if (user_s != null) {
+            model.addAttribute("nickname", user_s.getName());
+        }
+
         return "article-save";
 
     }
